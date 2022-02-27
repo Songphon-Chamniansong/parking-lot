8c4dda03780f31aaff3c4c5a1eb948d2b842941a
Songphon Chamniansong <songphon.cham@gmail.com>
2022-02-27 21:35:29 +0700

    remove test on DAL layer

--- src/repositories/parking-lot.repository.test.ts
+++ src/repositories/parking-lot.repository.test.ts
@@ -1,26 +1,26 @@
-import "reflect-metadata";
-import MongoMemoryServer from "mongodb-memory-server-core/lib/MongoMemoryServer";
+// import "reflect-metadata";
+// import MongoMemoryServer from "mongodb-memory-server-core/lib/MongoMemoryServer";
 import "jest";
-import { DbMock } from "../tests/utils/dbmock";
-import ParkingLot, { IParkingLot } from '../db/models/parking-lot.model';
+// import { DbMock } from "../tests/utils/dbmock";
+// import ParkingLot, { IParkingLot } from '../db/models/parking-lot.model';
 
 describe("Parking Lot Repository", () => {
-    let mongod: MongoMemoryServer;
+    // let mongod: MongoMemoryServer;
 
-    beforeAll(async () => {
-        mongod = await DbMock.initDbMock();
-    });
+    // beforeAll(async () => {
+    //     mongod = await DbMock.initDbMock();
+    // });
 
-    afterAll(async () => {
-        setTimeout(() => {
-            DbMock.stopDbMock();
-        }, 4000)
-    });
+    // afterAll(async () => {
+    //     setTimeout(() => {
+    //         DbMock.stopDbMock();
+    //     }, 4000)
+    // });
 
-    // Before each method we need to truncate all products from db
-    beforeEach(async () => {
-        ParkingLot.deleteMany({}).then();
-    });
+    // // Before each method we need to truncate all products from db
+    // beforeEach(async () => {
+    //     ParkingLot.deleteMany({}).then();
+    // });
 
     it("Should pass", async () => {
         expect(true).toEqual(true);
